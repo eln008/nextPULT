a2f5428ac96546c57beb32d9faa3e0b78cb42634
eln008 <140242882+eln008@users.noreply.github.com>
2023-08-07 15:01:28 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,68 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class Main {
-    private static int count = 0;
+    public static int count = 0;
     public static void main (String[] args){
         method();
 
     }
     public static  String method() {
+        String[] indeksKan= new String[]{"Welcome to the Samsung SMART TV ",
+
+                "+--------------------------------------------+\n" +
+                        "  channel name                           KTRK \n" +
+                        "                                              \n" +
+                        "channel number        1                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+                "+--------------------------------------------+\n" +
+                        "  channel name                     Ala Too 24 \n" +
+                        "                                              \n" +
+                        "channel number        2                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+
+
+                "+--------------------------------------------+\n" +
+                        "  channel name                       Balastan \n" +
+                        "                                              \n" +
+                        "channel number        3                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+
+                "+--------------------------------------------+\n" +
+                        "  channel name                           ELTR \n" +
+                        "                                              \n" +
+                        "channel number        4                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+
+                "+--------------------------------------------+\n" +
+                        "  channel name                     KTRK Sport \n" +
+                        "                                              \n" +
+                        "channel number        5                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+
+                "+--------------------------------------------+\n" +
+                        "  channel name                      Madaniyat \n" +
+                        "                                              \n" +
+                        "channel number         6                      \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+                "+--------------------------------------------+\n" +
+                        "  channel name                         Muzyka \n" +
+                        "                                              \n" +
+                        "channel number        7                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+",
+
+                "+--------------------------------------------+\n" +
+                        "  channel name                             NTS\n" +
+                        "                                              \n" +
+                        "channel number        8                       \n" +
+                        "                                              \n" +
+                        "+---------------------------------------------+"};
 
         System.out.println("              TV  пульт         \n" +
                 "+-------------------------------------+\n" +
@@ -22,8 +79,9 @@ public class Main {
 
 
         Scanner scan = new Scanner(System.in);
-        String mainNum = scan.next();
-        if (mainNum.equals("on")) {
+        String mainOn = scan.next();
+
+        if (mainOn.equals("on")) {
             System.out.println(
                     "+-------------------------------------+\n" +
                             "          WELCOME TO \n" +
@@ -40,196 +98,57 @@ public class Main {
                             "Каалаган каналга отуу учун каналдын номерин жазыныз\n" +
                             "Кийинки канлага отуу учун 'next' созун жазыныз\n" +
                             "Арткы каналга отуу учун 'back' созун жазыныз\n");
-            String pultNum = scan.next();
-            if (pultNum.equals("menu")) {
-                System.out.println("~~~~~~~~~~~All channels, number of channels 8~~~~~~~~~~\n" +
-                        "-----------------------------\n" +
-                        "channel name             KTRK\n" +
-                        "                             \n" +
-                        "   channel number          1 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name       Ala Too 24\n" +
-                        "                             \n" +
-                        "   channel number          2 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name         Balastan\n" +
-                        "                             \n" +
-                        "   channel number          3 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name             ELTR\n" +
-                        "                             \n" +
-                        "   channel number          4 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name       KTRK Sport\n" +
-                        "                             \n" +
-                        "   channel number          5 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name        Madaniyat\n" +
-                        "                             \n" +
-                        "   channel number          6 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name           Muzyka\n" +
-                        "                             \n" +
-                        "   channel number          7 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "channel name              NTS\n" +
-                        "                             \n" +
-                        "   channel number          8 \n" +
-                        "                             \n" +
-                        "-----------------------------\n" +
-                        "+-------------------------------------+\n" +
-                        "|      off                   on       |\n" +
-                        "|        <-back  (menu)  next->       |\n" +
-                        "|         1      2     3     4        |\n" +
-                        "+-------------------------------------|\n" +
-                        "Телевизорду куйгузуу учун 'on' созун жазыныз\n" +
-                        "Телевизорду очуруу учун 'off' созун жазыныз \n" +
-                        "Менюга кируу учун 'menu' созун жазыныз \n" +
-                        "Каалаган каналга отуу учун каналдын номерин жазыныз\n" +
-                        "Кийинки канлага отуу учун 'next' созун жазыныз\n" +
-                        "Арткы каналга отуу учун 'back' созун жазыныз\n");
-
-                String[] indeksKan= new String[]{"Welcome to the Samsung SMART TV ",
-                        "+--------------------------------------------+\n" +
-                                "  channel name                           KTRK \n" +
-                                "                                              \n" +
-                                "channel number        1                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-                "+--------------------------------------------+\n" +
-                                "  channel name                     Ala Too 24 \n" +
-                                "                                              \n" +
-                                "channel number        2                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-
-
-                        "+--------------------------------------------+\n" +
-                                "  channel name                       Balastan \n" +
-                                "                                              \n" +
-                                "channel number        3                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-
-                        "+--------------------------------------------+\n" +
-                                "  channel name                           ELTR \n" +
-                                "                                              \n" +
-                                "channel number        4                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-
-                        "+--------------------------------------------+\n" +
-                                "  channel name                     KTRK Sport \n" +
-                                "                                              \n" +
-                                "channel number        5                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-
-                        "+--------------------------------------------+\n" +
-                                "  channel name                      Madaniyat \n" +
-                                "                                              \n" +
-                                "channel number         6                      \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-                        "+--------------------------------------------+\n" +
-                                "  channel name                         Muzyka \n" +
-                                "                                              \n" +
-                                "channel number        7                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+",
-
-                        "+--------------------------------------------+\n" +
-                                "  channel name                             NTS\n" +
-                                "                                              \n" +
-                                "channel number        8                       \n" +
-                                "                                              \n" +
-                                "+---------------------------------------------+"};
-                int menuNum = scan.nextInt();
-                if (menuNum == 1) {
-                    System.out.println(indeksKan[1]);
-                    count=1;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 2) {
-                    System.out.println(indeksKan[2]);
-                    count =2;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 3) {
-                    System.out.println(indeksKan[3]);
-                    count = 3;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 4) {
-                    System.out.println(indeksKan[4]);
-                    count=4;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 5) {
-                    System.out.println(indeksKan[5]);
-                    count=5;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 6) {
-                    System.out.println(indeksKan[6]);
-                    count=6;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 7) {
-                    System.out.println(indeksKan[7]);
-                    count=7;
-                }else if (menuNum==0){
-                    count++;
-
-                    System.out.println(indeksKan[count]);
-                }
-                if (menuNum == 8) {
-                    System.out.println(indeksKan[8]);
-
-                }else if (menuNum==0){
-                    count++;
-                    System.out.println(indeksKan[count]);
-                }
-
-
-
-
-
-            }else if(pultNum.equals("off")){
-                System.exit(0);
-            } else System.out.println(" Error ");
-
-
-        }else if(mainNum.equals("off")){
+        }else if(mainOn.equals("off")){
             System.exit(0);
         }
 
+            String mainMenu = scan.next();
+            if (mainMenu.equals("menu")) {
+                for (int i = 0; i < indeksKan.length; i++) {
+                    System.out.println(indeksKan[i]);
+                }
+            } else if(mainMenu.equals("off")){
+            System.exit(0);
+                } else System.out.println(" Error ");
 
-        return method();
+
+            String mainNum = scan.next();
+             if (mainNum.equals(1) ) {
+            System.out.println(indeksKan[1]);
+            count = 1;
+            }else if (mainNum.equals(2)) {
+                    System.out.println(indeksKan[2]);
+                    count = 2;
+            }else if (mainNum.equals(3)) {
+                System.out.println(indeksKan[3]);
+                count = 3;
+            }else if (mainNum.equals(4)) {
+                System.out.println(indeksKan[4]);
+                count = 4;
+            }else if (mainNum.equals(5)) {
+                System.out.println(indeksKan[5]);
+                count = 5;
+            }else if (mainNum.equals(6)) {
+                System.out.println(indeksKan[6]);
+                count = 6;
+            }else if (mainNum.equals(7)) {
+                System.out.println(indeksKan[7]);
+                count = 7;
+            }else if (mainNum.equals(8)) {
+                System.out.println(indeksKan[8]);
+
+            }else if (mainNum.equals("next")) {
+                count = (count + 1) % indeksKan.length;
+                System.out.println("Switching to channel: " + indeksKan[count]);
+                method();
+             }
+
+
+       return method();
+
+
+
+
     }
 }
+
